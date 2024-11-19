41e9f6772e0e412cd82b24471f5dd73f86e3ccbb
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-11-19 11:51:12 -0800

    Added more hobbies case

--- file_reader.js
+++ file_reader.js
@@ -6,7 +6,28 @@ fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
     fs.readFile("./age.txt", "utf-8", (err, data3) => {
       fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
         const hobbies = JSON.parse(data4);
-        console.log(`${data1} ${data2} is ${data3} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`);
+        let be = ""
+
+        if(hobbies.length === 1) {
+          be = "is";
+        } else {
+          be = "are";
+        }
+
+        let stringHobbies = "";
+        for(let i = 0; i < hobbies.length; i++) {
+          if(i == 0) {
+            stringHobbies = hobbies[i];
+          } else if(i == (hobbies.length - 1)) {
+            stringHobbies += ` and ${hobbies[i]}`;
+          } else {
+            stringHobbies += `, ${hobbies[i]}`;
+          }
+        }
+
+        // console.log(`${data1} ${data2} is ${data3} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`);
+
+        console.log(`${data1} ${data2} is ${data3} years old and his hobbies ${be} ${stringHobbies}`);
       })
     })
   })
