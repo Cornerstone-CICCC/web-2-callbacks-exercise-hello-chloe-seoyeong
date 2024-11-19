ca90f27443a9fba00f7d7807f3594d1946d21451
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-11-19 11:41:20 -0800

    Completed callbacks exercise

--- file_reader.js
+++ file_reader.js
@@ -2,4 +2,12 @@ const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
   // your code here
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    fs.readFile("./age.txt", "utf-8", (err, data3) => {
+      fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+        const hobbies = JSON.parse(data4);
+        console.log(`${data1} ${data2} is ${data3} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`);
+      })
+    })
+  })
 })
